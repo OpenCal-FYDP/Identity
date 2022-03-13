c716d913e83e57a19fd56beace32f5b42b9e1b64
Jonathan Sun <jspsun@gmail.com>
2022-03-13 06:43:17 -0400

    Add cors

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.17
 
 require (
 	github.com/aws/aws-sdk-go v1.43.15
+	github.com/rs/cors v1.8.2
 	github.com/stretchr/testify v1.7.0
 	github.com/twitchtv/twirp v8.1.1+incompatible
 	google.golang.org/protobuf v1.27.1
@@ -11,11 +12,10 @@ require (
 
 require (
 	github.com/davecgh/go-spew v1.1.0 // indirect
-	github.com/golang/protobuf v1.5.0 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/kr/pretty v0.1.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
-	golang.org/x/oauth2 v0.0.0-20220309155454-6242fa91716a // indirect
-	google.golang.org/appengine v1.6.6 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
 )
--- main.go
+++ main.go
@@ -1,15 +1,41 @@
 package main
 
 import (
+	"fmt"
 	"github.com/OpenCal-FYDP/Identity/internal/service"
 	"github.com/OpenCal-FYDP/Identity/internal/storage"
 	"github.com/OpenCal-FYDP/Identity/rpc"
+	"github.com/rs/cors"
 	"log"
+	"math/rand"
 	"net/http"
+	"os"
 )
 
+func randomString(n int) string {
+	var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+
+	s := make([]rune, n)
+	for i := range s {
+		s[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(s)
+}
+
 func main() {
+	secret := os.Getenv("SECRET")
+	if secret == "" {
+		secret = randomString(128)
+		fmt.Printf("Randomly Generated Secret: %s\n", secret)
+	}
+	corsWrapper := cors.New(cors.Options{
+		AllowedOrigins: []string{"http://localhost:3000"},
+		AllowedMethods: []string{"POST"},
+		AllowedHeaders: []string{"Content-Type"},
+	})
+
 	svc := service.New(storage.New())
 	server := rpc.NewIdentityServiceServer(svc)
-	log.Fatal(http.ListenAndServe(":8080", server))
+	handler := corsWrapper.Handler(server)
+	log.Fatal(http.ListenAndServe(":8080", handler))
 }
